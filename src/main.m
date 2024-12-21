f6a9d0e83a6e44bb2e251b15a0bf57428c33ce09
ehsan1381 <ehsan.tc.evol@gmail.com>
2024-12-21 15:38:21 +0330

    minor changes tic - toc lines added to measure exxecution time for main script pcode files added

--- src/main.m
+++ src/main.m
@@ -3,6 +3,8 @@
 
 Nterms = input("Truncation point ");
 
+tic;
+
 % computing approximation error for different values of lambda
 LambdaArr = linspace(LRangeStart, LRangeEnd, LArrSize);
 
@@ -15,12 +17,10 @@ MainHandle = @(lambda)(Execute(lambda, Nterms, Polys, Facts));
 ErrorArr = zeros([LArrSize, 1]);
 ApproxArr = zeros([LArrSize, 1]);
 
-
-
-
 for i = 1:LArrSize
   lambda = LambdaArr(i);
   Approx = MainHandle(lambda);
   ErrorArr(i) = pi-Approx;
   ApproxArr(i) = Approx;
 end
+toc;
