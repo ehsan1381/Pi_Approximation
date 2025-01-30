87e6e729b0d83ba40a8c8b75e997bad747728639
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-01-30 16:36:41 +0330

    bisection updated this implementation was copied from ehsan1381/NA_Project_2 and is better overall

--- matlab/src/Bisection.m
+++ matlab/src/Bisection.m
@@ -1,52 +1,31 @@
-function [Steps] = Bisection(f, Start, End, TOL)
+function [ Approximation ] = Bisection(f, Start, End, TOL)
 
-    N_0 = ceil( ( log( End - Start ) - log( TOL ) ) / log( 2 ) )
-    if 2 * eps >= TOL
-        disp(['The bisection method will not be able to produce results accurate to ' num2str(TOL)]);
-        return;
+  % This N_0 is computed using the error bound formula for bisection method
+  N_0 = ceil( ( log( End - Start ) - log( TOL ) ) / log( 2 ) );
+
+  % to prevent computatio from happening in case requested precision is too high
+  if 2 * eps > TOL
+    error('%s is less then twice the computer epsilon. Bisection method cannot produce such accurate results', num2str(TOL, 15));
+  end
+
+  FA = f(Start);
+  LoopCounter = 1;
+  Approximation = 0;
+
+  % loop capped at max number of steps required for algorithm to produce the approximation
+  while LoopCounter <= N_0
+    Approximation = (Start + End) / 2;
+
+    % in case start, end or midpoint are good approximations
+    FP = f(Approximation);
+    LoopCounter = LoopCounter + 1;
+
+    if FA * FP > 0
+      Start = Approximation;
+      FA = FP;
+    else
+      End=Approximation;
     end
 
-    tic;
-    Steps = zeros([N_0 6]);
-    index = 1;
-    FA = f(Start);
-    fprintf('index\tStart\tEnd\tFA\tmiddle point\tFP\n');
-    while index <= N_0
-
-        middle_point = (End-Start) / 2;
-        Approximation = Start + middle_point;
-
-        FP = f(Approximation);
-        Steps(index, :) = [index;Start;End;FA;Approximation;FP];
-        if FA == 0 || f(End) == 0
-            if FA == 0
-                Approximation = Start;
-
-            else
-                Approximation = End;
-            end
-            disp(Approximation);
-            toc;
-            return ;
-        end
-
-        if (FP == 0) || abs(middle_point) < TOL
-            disp(Approximation);
-            toc;
-            return ;
-        end
-        index = index + 1;
-
-        if sign(FA) * sign(FP) > 0
-            Start = Approximation;
-            FA = FP;
-        else
-            End=Approximation;
-        end
-
-
-    end
-
-    disp(["Failure after ", num2str(N_0)]);
-    toc;
+  end
 end
